2f1a349262c49a463e19f5bb988975da9ae776a3
Bevis Pei <ppf@jiumao.org>
2020-07-31 16:39:09 +0800

    Update check_error.sh

--- check_error.sh
+++ check_error.sh
@@ -1,3 +1,2 @@
 #!/usr/bin/env bash
-ls ~/logs
-if [ $(find ~/logs -name "*.log" | xargs grep "ERROR" |wc -l) -gt 0 ]; then exit 1; fi
+if [ $(find ~/logs/seata -name "*.log" | xargs grep "ERROR" |wc -l) -gt 0 ]; then exit 1; fi
